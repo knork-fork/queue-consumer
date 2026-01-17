7cc3454d837efd2b005951d6abed369086c945fd
Luka Knezic <luka@giscloud.com>
2026-01-17 05:37:15 +0100

    Add command for testing dummy response

---
+++ src/Command/DispatchDummyResponseRetrievalCommand.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Message\GenericMessage;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+// Try out with: docker-compose exec consumer php bin/console app:dispatch-dummy-response
+// tail -f var/logs/php-fpm/php_dummy_response.log to see output
+#[AsCommand(name: 'app:dispatch-dummy-response')]
+final class DispatchDummyResponseRetrievalCommand extends Command
+{
+    public function __construct(private MessageBusInterface $bus)
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->bus->dispatch(new GenericMessage(
+            jobName: 'dummy-response-retrieval',
+            payload: [
+                'query_url_key' => 'value1',
+                'json_1' => 'value3',
+                'json_2' => 'value4',
+            ]
+        ));
+        $output->writeln('Dispatched');
+
+        return Command::SUCCESS;
+    }
+}
